9caae4d0cdcc36524982b99e63f7d1583111000e
Elyas <elyasarkint@gmail.com>
2024-09-15 14:26:08 +0200

    modified server

--- server/server.js
+++ server/server.js
@@ -5,11 +5,12 @@ const morgan = require("morgan");
 const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-const DEV_ENV = "http://localhost:3000";
+//const DEV_ENV = "http://localhost:3000";
 const PROD_ENV = "https://fakeshop-client-o4mz.onrender.com";
 app.use(cors({ origin: PROD_ENV, credentials: true }));
 // DB
@@ -35,6 +36,18 @@ app.use("/api/orders", orderRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+
+// see if production works well.
+// cd client and run npm run build
+
+// if (process.env.NODE_ENV === "prod") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(path.resolve(), "client", "build", "index.html")); //relative path
+//   });
+// } else {
+//  res.send("API is running...");
+// }
 
 // STRIPEs
 const stripePaymentRouter = require("./route/stripePaymantRoute");
